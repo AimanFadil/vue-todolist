5453e70d673196a53cfad182df8b908b11241394
AimanFadil <aimanbello2001@gmail.com>
2023-12-13 16:06:11 +0100

    1a parte

--- js/script.js
+++ js/script.js
@@ -3,6 +3,44 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            lista_do: [
+                {
+                   text : 'Apri il Client',
+                   done : true,  
+                },
+                {
+                    text : 'Inserisci le credenziali',
+                    done : true,  
+                },
+                {
+                    text : 'Effettua il Login',
+                    done : true,  
+                },
+                {
+                    text : 'Apri il gioco',
+                    done : true,  
+                },
+                {
+                    text : 'Clicca la x in alto a destra',
+                    done : false,  
+                },
+                {
+                    text : 'Entra sulle impostazioni del profilo',
+                    done : false,  
+                },
+                {
+                    text : 'Clicca elimina account',
+                    done : false,  
+                },
+                {
+                    text : 'Disinstalla l app',
+                    done : false,  
+                },
+                {
+                    text : 'Vai a goderti la vita',
+                    done : false,  
+                },
+            ]
            
        } 
     },
